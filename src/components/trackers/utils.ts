07a9bac7ee5673ad4f30e32e5aa5dfee3921c211
anton-lunev <antonylunev@gmail.com>
2026-02-16 13:21:08 -0800

    fix recreactiogov provider

--- src/components/trackers/utils.ts
+++ src/components/trackers/utils.ts
@@ -10,7 +10,7 @@ export function parseCampingUrl(campingUrl: string): ParsedCampingUrl {
   const url = new URL(campingUrl);
 
   if (url.hostname.includes("reservecalifornia.com")) {
-    const pathParts = url.hash.split("/");
+    const pathParts = url.pathname.split("/");
     const campingId = pathParts[pathParts.length - 1];
     const parkId = pathParts[pathParts.length - 2];
     return { provider: CampProvider.RESERVE_CALIFORNIA, parkId, campingId };
--- src/server/trackers/providers/reserve-california/config.ts
+++ src/server/trackers/providers/reserve-california/config.ts
@@ -1,10 +1,12 @@
+const RDR_API =
+  "https://california-rdr.prod.cali.rd12.recreation-management.tylerapp.com/rdr";
+
 export const PROVIDER_CONFIG = {
   BASE_URL: "https://www.reservecalifornia.com",
-  API_URL: "https://calirdr.usedirect.com/RDR/rdr/search/grid",
-  CAMPGROUND_API_URL:
-    "https://calirdr.usedirect.com/RDR/rdr/fd/facilities/:campingId",
-  CAMPGROUND_URL:
-    "https://www.reservecalifornia.com/Web/Default.aspx#!park/:parkId/:campingId",
+  API_URL: `${RDR_API}/search/grid`,
+  CAMPGROUND_API_URL: `${RDR_API}/fd/facilities/:campingId`,
+  PARK_API_URL: `${RDR_API}/fd/citypark/:parkId`,
+  CAMPGROUND_URL: "https://www.reservecalifornia.com/park/:parkId/:campingId",
   IMG_URL:
-    "https://cali-content.usedirect.com/Images/California/ParkImages/Place/:parkId.jpg",
+    "https://www.reservecalifornia.com/images/California/ParkImages/Place/:parkId.jpg",
 } as const;
